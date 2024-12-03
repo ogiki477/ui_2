65da0ed3b252c8e8d59113fd7f38a1411135e7a4
Ogiki Moses Odera <omojick88moses@gmail.com>
2024-12-03 19:02:48 +0300

    Update OnboardingScreen.dart

--- lib/screens/OnboardingScreen.dart
+++ lib/screens/OnboardingScreen.dart
@@ -10,6 +10,31 @@ class Onboardingscreen extends StatefulWidget {
 class _OnboardingscreenState extends State<Onboardingscreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 20,
+              top: 20,
+            ),
+            child: InkWell(
+              onTap: () {}, 
+              child: const Text(
+                "Skip",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
